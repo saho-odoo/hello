0e131bf2d62968bc7af68c08123cd63ca2293d07
Samiul Hoque <saho@odoo.com>
2024-09-25 13:56:46 -0400

    hello uses greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author: Samiul Hoque (saho@odoo.com)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
